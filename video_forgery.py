ac4349e40bacc8bc1aff3da0a8184c66a7152a58
iskan <iskandergafaroff@gmail.com>
2023-04-22 15:41:40 +0400

    ref

--- video_forgery.py
+++ video_forgery.py
@@ -15,101 +15,104 @@ def get_video_name(path: str):
 if __name__ == '__main__':
     th = 3
     path = "C:\\Users\\iskan\\Desktop\\test_videos"
-    paths_to_video_paths = "C:\\Users\\iskan\\PycharmProjects\\VideoForgeryDetection\\utils\\video_paths"
+    paths_to_video_paths = "C:\\Users\\iskan\\PycharmProjects\\Video-Forgery-Detection\\utils\\video_paths"
     no_of_forgery = []
     video_name = []
     video_paths_collector = VideoPathsCollector(paths_to_video_paths)
     videos_paths = video_paths_collector.collect()
     all_manipulations = {}
-    excel_writer = ExcelWriter("C:\\Users\\iskan\\PycharmProjects\\VideoForgeryDetection\\results", 'results')
+    excel_writer = ExcelWriter("C:\\Users\\iskan\\PycharmProjects\\Video-Forgery-Detection\\results", 'results')
     for video_path in videos_paths:
         file = get_video_name(video_path)
-        if file == 'small_walkers.mp4':
-            print('Process ', file)
-            cap = cv2.VideoCapture(video_path)
-            ret, frame1 = cap.read()
+        print('Process ', file)
+        cap = cv2.VideoCapture(video_path)
+        ret, frame1 = cap.read()
+        try:
             prvs = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
-            hsv = np.zeros_like(frame1)
-            hsv[..., 1] = 255
-            frame_no = []
-            op_flow_per_frame = []
-            m = 1
-            f = 1
-            b = 1
-            a = frame1.size
-            s = np.arange(a)
-            frame_number = 0
-            while (1):
-                s = 0
-                frame_number = frame_number + 1
-                ret, frame2 = cap.read()
-                print('frame number: ', frame_number)
+        except BaseException:
+            print('ERROR ', video_path)
+            continue
+        hsv = np.zeros_like(frame1)
+        hsv[..., 1] = 255
+        frame_no = []
+        op_flow_per_frame = []
+        m = 1
+        f = 1
+        b = 1
+        a = frame1.size
+        s = np.arange(a)
+        frame_number = 0
+        while (1):
+            s = 0
+            frame_number = frame_number + 1
+            ret, frame2 = cap.read()
+            print('frame number: ', frame_number)
 
-                if ret == True:
-                    next = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
-                    flow = cv2.calcOpticalFlowFarneback(prvs, next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+            if ret == True:
+                next = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
+                flow = cv2.calcOpticalFlowFarneback(prvs, next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
 
-                    frame_no.append(m)
-                    m = m + 1
-                    mag, ang = cv2.cartToPolar(flow[..., 0], flow[..., 1])
-                    op_flow_1D = np.resize(mag, (1, a))
-                    for i in op_flow_1D[0]:
-                        s = s + i
-                    op_flow_per_frame.append(s)
-                    prvs = next
-                    b = b + 1
-                else:
-                    break
-            vrt_factor = []
-            vrt_factor_round_2 = []
-            j = 1
-            awq = []
-            vrt_factor.append(1)
-            for i in range(1, (m)):
-                awq.append(i)
-            for o in (range(m - 3)):
-                c = (2 * op_flow_per_frame[j]) / (op_flow_per_frame[(j - 1)] + op_flow_per_frame[(j + 1)])
-                vrt_factor.append(c)
-                j = j + 1
-            vrt_factor.append(1)
-            for i in vrt_factor:
-                i = round(i, 2)
-                vrt_factor_round_2.append(i)
-            sum = np.sum(vrt_factor_round_2)
-            mean = (sum * 1.0) / (b)
+                frame_no.append(m)
+                m = m + 1
+                mag, ang = cv2.cartToPolar(flow[..., 0], flow[..., 1])
+                op_flow_1D = np.resize(mag, (1, a))
+                for i in op_flow_1D[0]:
+                    s  = s + i
+                op_flow_per_frame.append(s)
+                prvs = next
+                b = b + 1
+            else:
+                break
+        vrt_factor = []
+        vrt_factor_round_2 = []
+        j = 1
+        awq = []
+        vrt_factor.append(1)
+        for i in range(1, (m)):
+            awq.append(i)
+        for o in (range(m - 3)):
+            c = (2 * op_flow_per_frame[j]) / (op_flow_per_frame[(j - 1)] + op_flow_per_frame[(j + 1)])
+            vrt_factor.append(c)
+            j = j + 1
+        vrt_factor.append(1)
+        for i in vrt_factor:
+            i = round(i, 2)
+            vrt_factor_round_2.append(i)
+        sum = np.sum(vrt_factor_round_2)
+        mean = (sum * 1.0) / (b)
 
-            mean = round(mean, 3)
-            y = 0
-            poi = []
-            for i in vrt_factor_round_2:
-                y = y + ((i - mean) * (i - mean))
-            st = (y * 1.0) / (b)
-            st = round(st, 3)
-            fg = math.sqrt(2 * (22 / 7) * st)
-            anamoly_score = []
-            st = math.sqrt(st)
-            for i in vrt_factor_round_2:
-                kj = abs((i - mean))
-                df = (kj * 1.0) / st
-                anamoly_score.append(df)
-            bv = 0
-            anomalies = []
-            print('anomaly score!: ', anamoly_score)
-            for i in range(len(anamoly_score)):
-                if anamoly_score[i] > th:
-                    anomaly = Anomaly(anamoly_score[i], i)
-                    anomalies.append(anomaly)
-                    bv = bv + 1
-            no_of_forgery.append(bv)
-            video_name.append(file)
-            all_manipulations[file] = anomalies
-            plt.title('Video forgery')
-            plt.xlabel('Frame Number')
-            plt.ylabel('Anomly Score')
-            plot_name = file + '.png'
-            plt.plot(frame_no, anamoly_score)
-            plt.savefig(plot_name)
-            plt.close()
-            cv2.destroyAllWindows()
-            cap.release()
-    excel_writer.write(all_manipulations, False)
+        mean = round(mean, 3)
+        y = 0
+        poi = []
+        for i in vrt_factor_round_2:
+            y = y + ((i - mean) * (i - mean))
+        st = (y * 1.0) / (b)
+        st = round(st, 3)
+        fg = math.sqrt(2 * (22 / 7) * st)
+        anamoly_score = []
+        st = math.sqrt(st)
+        for i in vrt_factor_round_2:
+            kj = abs((i - mean))
+            df = (kj * 1.0) / st
+            anamoly_score.append(df)
+        bv = 0
+        anomalies = []
+        print('anomaly score!: ', anamoly_score)
+        for i in range(len(anamoly_score)):
+            if anamoly_score[i] > th:
+                anomaly = Anomaly(anamoly_score[i], i)
+                anomalies.append(anomaly)
+                bv = bv + 1
+        no_of_forgery.append(bv)
+        video_name.append(file)
+        all_manipulations[file] = anomalies
+        plt.title('Video forgery')
+        plt.xlabel('Frame Number')
+        plt.ylabel('Anomly Score')
+        plot_name = file + '.png'
+        plt.plot(frame_no, anamoly_score)
+        plt.savefig(plot_name)
+        plt.close()
+        cv2.destroyAllWindows()
+        cap.release()
+    # excel_writer.write(all_manipulations, False)
